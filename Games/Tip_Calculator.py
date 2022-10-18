b18eafd30deb8cde36961f3e5cb9b068e408d5c8
Ghanwat Kanifnath Adinath <110013661+kanifnathghanwat@users.noreply.github.com>
2022-10-18 23:08:58 +0530

    Update Tip_Calculator.py

--- Games/Tip_Calculator.py
+++ Games/Tip_Calculator.py
@@ -1,1 +1,28 @@
+'''
+Title: Tip Calculator
+Programming language: Python
+Developer name: Kanifnath Ghanwat
+Guid by: Amol Bhavsar Sir
+Date: 20/07/2022
+'''
 
+print("***** Tip Calculator *****")
+amount=int(input("Enter your total bill amount: "))  # Total amount
+tip=int(input("Enter Tip percent: "))                # Tip in percent
+person=int(input("Total number of Person: "))
+tip=(amount*tip)/100                                 # It gives total tip amount
+amount=amount+tip                                    # Add tip in total amount
+amount=amount/person                                 # Divide total amount in number of persons
+amount = round(amount, 2)                            # Rounds amount upto 2 decimal
+print(f"Each Person will pay: {amount}")             # It print total bill of each person
+
+'''
+output:
+
+***** Tip Calculator *****
+
+Enter your total bill amount: 2000
+Enter Tip percent: 15
+Total number of Person: 5
+Each Person will pay: 460.
+'''
